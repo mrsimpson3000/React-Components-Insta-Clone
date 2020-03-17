5af7bbf40db5d7abf86e1f6b51a6673ce83bb855
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-17 18:17:23 -0400

    Styled the comment box so it no longer exceeds its parent. Hopefully that's enough to hit MVP

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -6,10 +6,15 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
+  const [comment] = useState(props.comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comment.map((userComment, index) => (
+        <Comment comment={userComment} key={index} />
+      ))}
+
       <CommentInput />
     </div>
   );
