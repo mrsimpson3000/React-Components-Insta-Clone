10c7e1eb73d2f9b09322d300a72da0aee7f4e8cb
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-17 15:44:17 -0400

    Initial Commit. Struggling today.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ Not all files need code added.
 Look at each file to see where you need to pass props or add code 
 */
 import React from "react";
+import SearchBarContainer from "./components/SearchBar/SearchBarContainer";
+import PostsPage from "./components/PostsContainer/PostsPage";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
 
-
 const App = () => {
-
   return (
-    <div className="App">
-      {/* Add imported components here to render them */}
+    <div className='App'>
+      <SearchBarContainer />
+      <PostsPage />
     </div>
   );
 };
