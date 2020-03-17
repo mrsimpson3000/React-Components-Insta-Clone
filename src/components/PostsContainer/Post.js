59f5dbee020282a17a16e5ea60fb94f34aa540b4
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-17 17:38:49 -0400

    Likes now work

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -8,6 +8,10 @@ import "./Posts.css";
 
 // pass props in this file to
 const Post = props => {
+  const [likes, setLikes] = useState(props.postContent.likes);
+  const addLikes = () => {
+    setLikes(likes + 1);
+  };
   // set up state for the likes
 
   return (
@@ -23,7 +27,7 @@ const Post = props => {
           src={props.postContent.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection addLikes={addLikes} likes={likes} />
       <CommentSection
         postId={props.postContent.imageUrl}
         comments={props.postContent.comments}
